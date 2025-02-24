967cd7561f977a78b94c7a4889e8fa7810359b48
phuc nguyen <dangphucnguyen20112005@gmail.com>
2025-02-24 19:03:14 +0700

    set up du an

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom"
-import Header from "./components/layout/appHeader"
+import Header from "components/layout/appHeader"
 
 function Layout() {
     return (
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,27 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import "styles/global.scss"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Layout from './layout'
+import Layout from '@/layout'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import BookPage from './pages/client/book';
-import AboutPage from './pages/client/about';
-import LoginPage from './pages/client/auth/login';
-import RegisterPage from './pages/client/auth/register';
+import BookPage from 'pages/client/book';
+import AboutPage from 'pages/client/about';
+import LoginPage from 'pages/client/auth/login';
+import RegisterPage from 'pages/client/auth/register';
+import HomePage from "./pages/client/home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
       {
         path: "/book",
         element: <BookPage />,
